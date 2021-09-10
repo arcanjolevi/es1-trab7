a379d822bf4e1777efbce243a076c635659c2019
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-09-09 21:24:46 -0300

    rendimento

--- backend/src/main/java/com/main/controller/UsRendimento.java
+++ backend/src/main/java/com/main/controller/UsRendimento.java
@@ -1,5 +1,51 @@
 package com.main.controller;
 
+import com.main.bo.pessoa.Contribuinte;
+import com.main.bo.pessoa.Empresa;
+import com.main.bo.pessoa.Rendimento;
+import com.main.model.database.DbConnection;
+import com.main.model.database.DbContribuinte;
+import com.main.model.database.DbEmpresa;
+
 public class UsRendimento {
-  
+  private DbConnection connection;
+  private DbRendimento conRendimento;
+  private DbContribuinte conContribuinte;
+  private DbEmpresa conEmpresa;
+
+  public UsEmpresa() {
+    try {
+      this.connection = new DbConnection("root", "123");
+      this.conRendimento = new DbRendimento(this.connection);
+      this.conContribuinte = new DbContribuinte(this.connection);
+      this.conEmpresa = new DbEmpresa(this.connection);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public int Cadastrar(Rendimento rendimento, String cnpj, String cpf) throws Exception {
+    if (rendimento.getInss() == null)
+      throw new Error("Falta de dados na estrutura, Inss.");
+    if (rendimento.getIrrf() == null)
+      throw new Error("Falta de dados na estrutura, Irrf.");
+    if (rendimento.getValor() == null)
+      throw new Error("Falta de dados na estrutura, Valor.");
+
+    Empresa empresa = this.conEmpresa.get(cnpj);
+    rendimento.setEmpresa(empresa);
+    Contribuinte contribuinte = this.conContribuinte.get(cpf);
+
+    return this.conRendimento.insert(rendimento, cpf);
+  }
+
+  public Rendimento Consultar(Integer id) throws SQLException, Exception {
+    try {
+      return this.conRendimento.get(id);
+    } catch (SQLException sqlErr) {
+      throw sqlErr;
+    } catch (Exception Err) {
+      throw Err;
+    }
+  }
 }
