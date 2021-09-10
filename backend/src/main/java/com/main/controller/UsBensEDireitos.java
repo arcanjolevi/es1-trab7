4201ec6eaf0d3791ed02c4f98daed66d02ef345f
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-09-10 09:53:27 -0300

    a

--- backend/src/main/java/com/main/controller/UsBensEDireitos.java
+++ backend/src/main/java/com/main/controller/UsBensEDireitos.java
@@ -1,5 +1,46 @@
 package com.main.controller;
 
+import java.sql.SQLException;
+
+import com.main.bo.bensedireitos.BensEDireitos;
+import com.main.model.database.DbConnection;
+import com.main.model.database.DbContribuinte;
+import com.main.model.database.DbBensEDireitos;
+
 public class UsBensEDireitos {
-  
+  private DbConnection connection;
+  private DbBensEDireitos conBensEDireitos;
+  private DbContribuinte conContribuinte;
+
+  public UsBensEDireitos() {
+    try {
+      this.connection = new DbConnection("root", "123");
+      this.conBensEDireitos = new DbBensEDireitos(this.connection);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public int Cadastrar(BensEDireitos bensEDireitos, String cpf) throws Exception {
+    if (bensEDireitos.getValor() == null)
+      throw new Exception("Falta de dados na estrutura, Valor.");
+    if (bensEDireitos.getTipoBem().getNome() == null)
+      throw new Exception("Falta de dados na estrutura, Tipo Bem.");
+    if (bensEDireitos.getTipoBem().getDescricao() == null)
+      throw new Exception("Falta de dados na estrutura, Descrição.");
+
+    this.conContribuinte.get(cpf);
+
+    return this.conBensEDireitos.insert(bensEDireitos, cpf);
+  }
+
+  public BensEDireitos Consultar(Integer id) throws SQLException, Exception {
+    try {
+      return conBensEDireitos.get(id);
+    } catch (SQLException sqlErr) {
+      throw sqlErr;
+    } catch (Exception Err) {
+      throw Err;
+    }
+  }
 }
